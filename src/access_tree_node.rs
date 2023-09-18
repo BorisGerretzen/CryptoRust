581736114c96f9d819de76245cdbc6380c9e2dc1
Boris Gerretzen <borisgerretzen@gmail.com>
2023-09-18 13:03:53 +0200

    magic

--- src/access_tree_node.rs
+++ src/access_tree_node.rs
@@ -1,11 +1,22 @@
-pub enum TreeNode {
+use rand::Rng;
+use rabe_bn::Fr;
+use crate::access_tree_node::TreeOperator::{Or};
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TreeOperator {
+    Or,
+}
+
+pub enum AccessTree {
     Operator {
         operator: TreeOperator,
-        left: Box<TreeNode>,
-        right: Box<TreeNode>,
+        left: Box<AccessTree>,
+        right: Box<AccessTree>,
+        value: Option<Fr>,
     },
     Leaf {
-        attribute: String
+        attribute: String,
+        value: Option<Fr>,
     },
 }
 
@@ -13,23 +24,45 @@ pub trait GetAttributes {
     fn get_attributes(&self) -> Vec<String>;
 }
 
-impl GetAttributes for TreeNode {
+impl GetAttributes for AccessTree {
     fn get_attributes(&self) -> Vec<String> {
         match self {
-            TreeNode::Operator { left, right, .. } => {
+            AccessTree::Operator { left, right, .. } => {
                 let mut left_attributes = left.get_attributes();
                 let mut right_attributes = right.get_attributes();
                 left_attributes.append(&mut right_attributes);
                 left_attributes
             }
-            TreeNode::Leaf { attribute } => {
+            AccessTree::Leaf { attribute, .. } => {
                 vec![attribute.clone()]
             }
         }
     }
 }
 
-pub enum TreeOperator {
-    And,
-    Or,
+pub trait AssignValues {
+    fn assign_values<R: Rng>(&self, s: Fr, s_i: Option<Fr>, rng: &mut R);
+}
+
+impl AssignValues for AccessTree {
+    fn assign_values<R: Rng>(&self, s: Fr, s_i: Option<Fr>, rng: &mut R) {
+        match self {
+            AccessTree::Operator { left, right, mut value, operator } => {
+                if s_i.is_some() {
+                    value = s_i;
+                }
+
+                if operator == &Or {
+                    left.assign_values(s, None, rng);
+                    right.assign_values(s, None, rng);
+                }
+            }
+            AccessTree::Leaf { .. } => {}
+        }
+    }
+}
+
+pub struct EncryptionTree {
+    pub node: Box<AccessTree>,
+    pub value: Fr,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,53 @@
 mod access_tree_node;
 
-extern crate substrate_bn;
+extern crate rabe_bn;
 extern crate rand;
 
-use substrate_bn::{Group, Fr, G1, G2, pairing};
+use std::ops::Mul;
+use rand::Rng;
+use rabe_bn::{Group, Fr, G1, G2, pairing, Gt};
+use rabe_bn::arith::U256;
 use access_tree_node::TreeOperator;
-use crate::access_tree_node::TreeNode::Operator;
-use crate::access_tree_node::TreeNode::Leaf;
+use crate::access_tree_node::{GetAttributes};
+use crate::access_tree_node::AccessTree::Operator;
+use crate::access_tree_node::AccessTree::Leaf;
 
 fn main() {
     let rng = &mut rand::thread_rng();
-    let g = G1::one();
-    let pair = pairing(g, G2::one());
 
-    // Access Tree
-    let _tree = Operator {
+    let g:G1 = rng.gen();
+    let g2:G2 = rng.gen();
+
+
+    let tree = Operator {
         operator: TreeOperator::Or,
         left: Box::from(Operator {
-            operator: TreeOperator::And,
-            left: Box::from(Leaf { attribute: "A".to_string() }),
-            right: Box::from(Leaf { attribute: "B".to_string() }),
+            operator: TreeOperator::Or,
+            left: Box::from(Leaf { attribute: "A".to_string(), value: None }),
+            right: Box::from(Leaf { attribute: "B".to_string(), value: None }),
+            value: None
         }),
         right: Box::from(Operator {
-            operator: TreeOperator::And,
-            left: Box::from(Leaf { attribute: "C".to_string() }),
-            right: Box::from(Leaf { attribute: "D".to_string() }),
+            operator: TreeOperator::Or,
+            left: Box::from(Leaf { attribute: "C".to_string(), value:None }),
+            right: Box::from(Leaf { attribute: "D".to_string(), value:None }),
+            value: None,
         }),
+        value: None,
     };
 
     // SETUP
     // Create a vector containing 3 strings
     let attributes = vec!["A", "B", "C", "D"];
-    let _n_attributes = attributes.len();
 
-    let mut small_t = Vec::new();
+    let mut small_t:Vec<Fr> = Vec::new();
     for _ in 0..attributes.len() {
-        small_t.push(Fr::random(rng));
+        small_t.push(rng.gen());
     }
 
-    // Generate alpha
-    let alpha = Fr::random(rng);
-
-    // e(g,g)^alpha
-    let _y = pair.pow(alpha);
+    let alpha:Fr = rng.gen();
+    let pair = pairing(g, g2);
+    let y = pair.pow(alpha);
 
     let mut big_t = Vec::new();
     for i in 0..attributes.len() {
@@ -50,19 +55,38 @@ fn main() {
     }
 
     // KEYGEN
-    let r = Fr::random(rng);
+    let my_attributes = vec!["A"];
+    let r = rng.gen();
+    let d_0 = g2 * (alpha - r);
 
-    // d_j = g * (r * (t_j)^-1)
-    let _d_0 = g * (alpha - r);
     let mut arr_d = Vec::new();
-    for i in 0..attributes.len() {
-        arr_d.push(g * (r * (small_t[i].inverse().unwrap())));
+    for i in 0..my_attributes.len() {
+        arr_d.push(g2 * (r * small_t[i].inverse().unwrap()));
     }
 
-    // ENCRYPT
-    let m = Fr::random(rng);
 
-    let s = Fr::random(rng);
-    let _c = g * s;
-    let _c_1 = pair.pow(alpha * s).pow(m);
+    // ENCRYPT
+    let m:Gt = rng.gen();
+    let s = rng.gen();
+    let c_0 = g * s;
+    let c_1 = m * y.pow(s);
+    let c_j = tree.get_attributes().iter().enumerate().map(|x| g * (small_t[x.0] * s)).collect::<Vec<G1>>();
+
+    // DECRYPT
+    let mut product= None;
+    for i in 0..my_attributes.len() {
+        if product == None {
+            product = Some(pairing(c_j[i], arr_d[i]));
+        }
+        else {
+            product = Some(product.unwrap() * pairing(c_j[i], arr_d[i]));
+        }
+    }
+
+    let _egsga = pairing(c_0, d_0) * product.unwrap();
+    let _m_prime = c_1 * _egsga.inverse();
+
+
+    // assert that m and _m_prime are equal
+    assert_eq!(m, _m_prime);
 }
